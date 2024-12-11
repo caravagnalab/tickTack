25b147f5ab2bc7915a28e1917d7e3abf5186b864
jovoni <65920542+jovoni@users.noreply.github.com>
2024-12-11 11:39:02 +0000

    Deploying to gh-pages from @ caravagnalab/tickTack@0868836691981d0e29624c2bdc3ab71222139f10 🚀

---
+++ reference/figures/make_logo.R
@@ -0,0 +1,17 @@
+
+hexSticker::sticker(
+  "man/figures/logo.png",
+  package = "tickTack",
+  s_x = 1,
+  s_y = .8,
+  s_width = .5,
+  s_height = .5,
+  p_size = 20,
+  p_y = 1.5,
+  p_color = "gray10",
+  h_fill = "gray95",
+  #p_family = "sans",
+  h_color = "gray10",
+  filename = "man/figures/tickTack.png",
+  dpi = 300, h_size = .5
+)
