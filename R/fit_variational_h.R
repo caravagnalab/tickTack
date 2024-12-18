3e519594f8aef8b015478be77773c85195ae47bb
saracoco <sara.cocomello@gmail.com>
2024-12-18 21:45:14 +0100

    modify fit_variational

--- R/fit_variational_h.R
+++ R/fit_variational_h.R
@@ -19,31 +19,47 @@
 
 fit_variational_h <- function(input_data, max_attempts = 2, initialization = NULL, INIT = TRUE, initial_iter = 100, grad_samples = 200, elbo_samples = 200, tolerance = 0.01) {
   # Load the Stan model
+  
+  get_model <- function(model_name) {
+    all_paths <- list(
+      "timing_betabinomial" = "mixture_CNA_timing_betabinomial.stan",
+      "timing_binomial" = "mixture_CNA_timing_binomial.stan",
+      "timing" = "mixture_CNA_timing.stan",
+      "clustering" = "clustering.stan",
+      "hierarchical" = "timing_mixed_simple.stan"
+    )
+    
+    if (!(model_name) %in% names(all_paths)) stop("model_name not recognized")
+    
+    model_path <- system.file("cmdstan", all_paths[[model_name]], package = "tickTack", mustWork = T)
+    tmp <- utils::capture.output(suppressMessages(model <- cmdstanr::cmdstan_model(model_path)))
+    model
+  }
+  
   model <- get_model("hierarchical") 
   # cmdstanr::cmdstan_model("../models/timing_mixed_simple.stan")
   best_elbo <- -Inf  # Start with the worst possible ELBO
   best_fit <- NULL  # To store the best model fit
   total_attempts <- 0
-
+  
   for (attempt in 1:max_attempts) {
     message("Attempt ", attempt, " of ", max_attempts)
-
+    
     # retries <- 0  # Initialize retries for each attempt
     fit_successful <- FALSE  # Reset success flag for each attempt
     iter <- initial_iter  # Reset iterations for each new attempt
-
+    
     retries <- 0
-
+    
     while (!fit_successful && retries < 3) {  # Set a max number of retries for each attempt
-
-      # message("Running inference, retry ", retries + 1)
-
+      message("Retries ", retries, " of ", 3)
+      
       # Increment total_attempts at the start of each retry
       total_attempts <- total_attempts + 1
-      retries <- retries + 1
-
+      
       result <- tryCatch({
         # Attempt variational inference
+        print(list(initialization))
         if (INIT == TRUE) {
           res <- model$variational(
             data = input_data,
@@ -57,9 +73,9 @@ fit_variational_h <- function(input_data, max_attempts = 2, initialization = NUL
             eval_elbo = 1,
             tol_rel_obj = tolerance
           )
-          print(res$init())
-
-
+          # print(res$init())
+          
+          
           # Shuffle and moderately perturb initialization values
           ###############################################################################################################################################################
           shuffled_taus <- sample(initialization$tau)
@@ -67,15 +83,15 @@ fit_variational_h <- function(input_data, max_attempts = 2, initialization = NUL
           perturbed_taus <- shuffled_taus + stats::rnorm(length(shuffled_taus), 0, 0.1) # Adjust the perturbation scale as needed
           perturbed_taus[perturbed_taus >= 0.88] <- 0.88  # Check for elements greater than 1 and replace them with 1 otherwise fit fails
           perturbed_taus[perturbed_taus <= 0] <- 0.07
-
+          
           initialization$tau = perturbed_taus
           # phi does not change
-
+          
           # Apply the perturbation to w as wells
           # print (paste0("w_init after inference: ",initialization$w," ",ncol(initialization$w) == 1))
           #######################################################################################################################################################################
-
-
+          
+          
         } else {
           res <- model$variational(                                                #getter?
             data = input_data,
@@ -89,7 +105,9 @@ fit_variational_h <- function(input_data, max_attempts = 2, initialization = NUL
             tol_rel_obj = tolerance
           )
         }
-
+        
+        
+        
         output_files <- res$latent_dynamics_files()                                 #getter?
         elbo_data <- utils::read.csv(output_files, header = FALSE, comment.char = "#")
         colnames(elbo_data) <- c("iter", "time_in_seconds", "ELBO")
@@ -100,34 +118,35 @@ fit_variational_h <- function(input_data, max_attempts = 2, initialization = NUL
           best_elbo <- elbo
           best_fit <- res
         }
-
+        
         message("ELBO for this run: ", elbo)
-
+        
         # Check for invalid log evaluations
         output_files <- res$output_files()                                           #getter? + implement condition
         fit_successful <- TRUE  # Mark this fit as successful
-
+        
       }, error = function(e) {
         message("An error occurred during inference: ", e$message)
-        # retries <- retries + 1  # Increment retry count
+        retries <- retries + 1  # Increment retry count
         fit_successful <- FALSE  # Mark fit as unsuccessful
         iter <- iter + 400
         tol_rel_obj <- tol_rel_obj * 5
         grad_samples <- grad_samples * 3
         elbo_samples <- elbo_samples * 3
         
+        # if it failed then       retries <- retries + 1 and fit_succ <- false else continue 
         
         NULL  # Ensure NULL is returned so loop can continue
       })
-
+      
       # No return from `tryCatch` - just continue the loop if not successful
     }
-
+    
     if (retries == 3) {
       message("Max retries reached for attempt ", attempt)
     }
   }
-
+  
   if (!is.null(best_fit)) {
     # message("Best ELBO after ", total_attempts, " attempts: ", best_elbo)
     return(best_fit)  # Return the result with the best ELBO
